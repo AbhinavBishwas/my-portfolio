913a9b229a5c66327173d834c44b71a3122614f5
abhiav bishwas <abhinav.biswas06@gmail.com>
2024-04-04 00:55:58 +0530

    smooth scroll

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { FaBars, FaTimes} from 'react-icons/fa'
-// import {Link} from 'react-router-dom'
+import {Link} from 'react-scroll'
 const Navbar = () => {
 const [nav,setnav] =useState(false);
   const links=[
@@ -37,7 +37,9 @@ const [nav,setnav] =useState(false);
 
       {links.map((link)=> (
         <li key={link.id} className="px-4 cursor-pointer hover:border-b-2  hover:inline hover:border-gray-400 hover:text-gray-300    capitalize font-medium text-gray-400 hover:scale-105 duration-200 text-xl" >
+          <Link to={link.link} smooth duration={500} >
           {link.link}
+          </Link>
         </li>   
       ))}
 
@@ -48,11 +50,15 @@ const [nav,setnav] =useState(false);
       {nav ? <FaTimes size={30}/> : <FaBars size={30}/>}
     </div>
 
-        {nav && (
-          <ul className="  flex flex-col justify-center items-center text-4xl top-0 absolute left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800  text-gray-300">
+        {nav  && (
+          <ul  className="  flex flex-col justify-center items-center text-4xl top-0 absolute left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800  text-gray-300">
         
           {links.map((link)=>(
-            <li key={link.id} className="py-6 cursor-pointer capitalize font-medium hover:scale-105 duration-200">{link.link}</li>
+            <li  key={link.id} className="py-6 cursor-pointer capitalize font-medium hover:scale-105 duration-200">
+              <Link onClick={()=> setnav(!nav)} to={link.link} smooth duration={500} >
+            {link.link}
+            </Link>
+            </li>
           ))}        
         </ul>
         )}
